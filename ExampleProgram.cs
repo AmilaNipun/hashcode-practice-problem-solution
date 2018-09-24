c3d876d7a2d953495cdd712a6175fcb04a0d24e9
Chembe Banda <cbthe1st@gmail.com>
2018-09-24 20:06:29 +0200

    Add files via upload
    first commit

---
+++ ExampleProgram.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+
+namespace hashcode
+{
+	class SmallClass
+	{
+		// predetermined integers from input file
+		static int R = 3;
+		static int C = 5;
+
+		static int[,] inttop;
+		static bool isFirstTime = true;
+		static int slices = 0;
+		static int i = 0;
+		static int[,] sliceSaver = new int[7, 4];
+
+		public static void Main (string[] args)
+		{
+			int score = 0;
+
+			//Instantiate a StreamReader to read from the text file.
+			StreamReader sr = new StreamReader (@"example.in");
+			// gets the first role from input file out of the way
+			char[] row1 = sr.ReadLine ().ToCharArray();
+			// string that stores all the elements from the input file (T and M)
+			string toppings = sr.ReadToEnd ();
+			// character array
+			char[] chartop = toppings.ToCharArray();
+			// integer array that converts T to 1, M to 0
+			inttop = new int[R,C];
+
+			// filling inttop with values
+			int z=0;
+			for (int x=0;x<R;x++) {
+				for (int y=0;y<C;y++) {
+					if (chartop [z] == 'T') {
+						inttop [x, y] = 1;
+					} else if (chartop [z] == 'M')
+						inttop [x, y] = 0;
+					else{
+						y--;
+					}
+					z++;
+				}
+			}
+
+			// 3x2 slices
+			int slicesCut = slicer (0, 0, 2, 1, 5, 6);
+			slices = slicesCut + slices;
+			score = slicesCut * 6;
+			Console.WriteLine (slices + " " + score + "pts");
+
+			// 3x1 slices
+			slicesCut = slicer (0, 0, 2, 0, 1, 3);
+			slices = slicesCut + slices;
+			score = score + slicesCut * 3;
+			Console.WriteLine (slices + " " + score + "pts");
+
+			documenter();
+			Console.WriteLine ("DONE");
+			Console.ReadKey ();
+		}
+	
+		// the method that creates the output file and writes to it
+		public static void documenter(){
+			Console.WriteLine ("Saving output file...");
+			for (int e=0;e<slices;e++) {
+				string line = sliceSaver[e,0] + " " + sliceSaver[e,1] + " " + sliceSaver[e,2] + " " + sliceSaver[e,3];
+				using (StreamWriter sw = new StreamWriter ("example_output.txt", true)) {
+					if (isFirstTime) {
+						sw.WriteLine (slices);
+						isFirstTime = false;
+					}
+					sw.WriteLine (line);
+				}
+			}
+		}
+
+		// the method that cuts the pizza into suitable slices
+		public static int slicer(int r1, int c1, int r2, int c2, int min, int max){
+			int sliceCell = 0;
+			int slice = 0;
+
+			for (int r=0;r<R-r2;r++) {
+				for (int c=0;c<C-c2;c++) {
+					for (int x=r1+r;x<=r2+r;x++) {
+						for (int y=c1+c;y<=c2+c;y++) {
+							if (inttop [x, y] == 1) {
+								sliceCell++;
+							}
+						}
+					}
+					if (sliceCell < max) {
+						if (sliceCell >= min) {
+							bool isSlice = checker (r1 + r, c1 + c, r2 + r, c2 + c);
+							if (isSlice)
+								slice++;
+						}
+					}
+					sliceCell = 0;
+				}
+			}
+			return slice;
+		}
+			
+		public static void saver(int r1, int c1, int r2, int c2) {
+			sliceSaver[i,0] = r1;
+			sliceSaver[i,1] = c1;
+			sliceSaver[i,2] = r2;
+			sliceSaver[i,3] = c2;
+			i++;
+		}
+
+		public static bool checker(int r1, int c1, int r2, int c2) {
+			bool check = true;
+			for (int a = 0; a <= i; a++) {
+				if (a == i)
+					break;
+				for (int r = r1; r <= r2; r++) {
+					for (int c = c1; c <= c2; c++) {
+						if ((sliceSaver [a, 0] <= r && r <= sliceSaver [a, 2]) && (sliceSaver [a, 1] <= c && c <= sliceSaver [a, 3])) {
+							check = false;
+						}
+					}
+				}
+			}
+			if (check)
+				saver (r1, c1, r2, c2);
+			return check;
+		}
+	}
+}
---
+++ SmallProgram.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+
+namespace hashcode
+{
+	public class SmallProgram
+	{
+		// predetermined integers from input file
+		static int R = 6;
+		static int C = 7;
+
+		static int[,] inttop;
+		static bool isFirstTime = true;
+		static int slices = 0;
+		static int i = 0;
+		static int[,] sliceSaver = new int[21, 4];
+
+		public static void Main (string[] args)
+		{
+			int score = 0;
+
+			//Instantiate a StreamReader to read from the text file.
+			StreamReader sr = new StreamReader (@"small.in");
+			// gets the first role from input file out of the way
+			char[] row1 = sr.ReadLine ().ToCharArray();
+			// string that stores all the elements from the input file (T and M)
+			string toppings = sr.ReadToEnd ();
+			// character array
+			char[] chartop = toppings.ToCharArray();
+			// integer array that converts T to 1, M to 0
+			inttop = new int[R,C];
+
+			// filling inttop with values
+			int z=0;
+			for (int x=0;x<R;x++) {
+				for (int y=0;y<C;y++) {
+					if (chartop [z] == 'T') {
+						inttop [x, y] = 1;
+					} else if (chartop [z] == 'M')
+						inttop [x, y] = 0;
+					else{
+						y--;
+					}
+					z++;
+				}
+			}
+
+			// 1x5 slices
+			int slicesCut = slicer (0, 0, 0, 4, 1, 5);
+			slices = slicesCut + slices;
+			score = slicesCut * 5;
+			Console.WriteLine (slices + " " + score + "pts");
+
+			// 4x1 slices
+			slicesCut = slicer (0, 0, 3, 0, 1, 4);
+			slices = slicesCut + slices;
+			score = score + slicesCut * 4;
+			Console.WriteLine (slices + " " + score + "pts");
+
+			documenter();
+			Console.WriteLine ("DONE");
+			Console.ReadKey ();
+		}
+
+		// the method that creates the output file and writes to it
+		public static void documenter(){
+			Console.WriteLine ("Saving output file...");
+			for (int e=0;e<slices;e++) {
+				string line = sliceSaver[e,0] + " " + sliceSaver[e,1] + " " + sliceSaver[e,2] + " " + sliceSaver[e,3];
+				using (StreamWriter sw = new StreamWriter ("small_output.txt", true)) {
+					if (isFirstTime) {
+						sw.WriteLine (slices);
+						isFirstTime = false;
+					}
+					sw.WriteLine (line);
+				}
+			}
+		}
+
+		// the method that cuts the pizza into suitable slices
+		public static int slicer(int r1, int c1, int r2, int c2, int min, int max){
+			int sliceCell = 0;
+			int slice = 0;
+
+			for (int r=0;r<R-r2;r++) {
+				for (int c=0;c<C-c2;c++) {
+					for (int x=r1+r;x<=r2+r;x++) {
+						for (int y=c1+c;y<=c2+c;y++) {
+							if (inttop [x, y] == 1) {
+								sliceCell++;
+							}
+						}
+					}
+					if (sliceCell < max) {
+						if (sliceCell >= min) {
+							bool isSlice = checker (r1 + r, c1 + c, r2 + r, c2 + c);
+							if (isSlice)
+								slice++;
+						}
+					}
+					sliceCell = 0;
+				}
+			}
+			return slice;
+		}
+
+		public static void saver(int r1, int c1, int r2, int c2) {
+			sliceSaver[i,0] = r1;
+			sliceSaver[i,1] = c1;
+			sliceSaver[i,2] = r2;
+			sliceSaver[i,3] = c2;
+			i++;
+		}
+
+		public static bool checker(int r1, int c1, int r2, int c2) {
+			bool check = true;
+			for (int a = 0; a <= i; a++) {
+				if (a == i)
+					break;
+				for (int r = r1; r <= r2; r++) {
+					for (int c = c1; c <= c2; c++) {
+						if ((sliceSaver [a, 0] <= r && r <= sliceSaver [a, 2]) && (sliceSaver [a, 1] <= c && c <= sliceSaver [a, 3])) {
+							check = false;
+						}
+					}
+				}
+			}
+			if (check)
+				saver (r1, c1, r2, c2);
+			return check;
+		}
+	}
+}
